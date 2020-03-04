99d759f85cca5e85409734441e86b2bdc4ab7e6b
Duncan Uruchima <duruchima1@gmail.com>
2020-03-04 17:19:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   array=string.split
   i=0
   array.each do |word|
-    reverse_array<<"#{word.reverse}"
+    reverse_array<< word.reverse
   end
   return reverse_array
 end
