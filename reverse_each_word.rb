34dd0a102000282a2b10fd5c42ec3b79aecc7ab0
Duncan Uruchima <duruchima1@gmail.com>
2020-03-04 17:17:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,8 @@
 def reverse_each_word(string)
   array=string.split
   i=0
-  reverse_array = array.each {|word| "#{word}.reverse"}
+  array.each do |word|
+    reverse_array="#{word.reverse}"
+  end
   return reverse_array
 end
