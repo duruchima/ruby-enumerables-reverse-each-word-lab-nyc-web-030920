0ab7d097fe8ec0b4565d67ab6348ea67c1fde690
Duncan Uruchima <duruchima1@gmail.com>
2020-03-04 17:49:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word(string)
   reversed_string = ""
   array.each do |word|
     word.to_s
-    text = " #{word}".reverse.strip
+    text = " #{word}".reverse.chomp
     reversed_string.concat text
   end
   return reversed_string
